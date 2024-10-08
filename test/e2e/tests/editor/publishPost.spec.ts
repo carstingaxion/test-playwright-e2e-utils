3ce37bfac2446cd606fb577fe249691d8c89d5a8
Carsten Bach <mail@carsten-bach.de>
2024-10-08 03:07:33 +0200

    Fix iframed locator

--- test/e2e/tests/editor/publishPost.spec.ts
+++ test/e2e/tests/editor/publishPost.spec.ts
@@ -18,7 +18,7 @@ test.describe('Publish posts in the Editor', () => {
 		await editor.publishPost(); // this is missing the force and doesnt work.
 		await page.reload();
 
-		await expect( await page.getByLabel('Add title') ).toHaveText('Change title to allow saving');
+		await expect( await page.frameLocator('iframe[name="editor-canvas"]').getByLabel('Add title') ).toHaveText('Change title to allow saving');
 	});
 
 });
